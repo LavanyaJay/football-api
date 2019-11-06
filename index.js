3c615be9b5c0a41c8d71d57105417e2ef1f71206
LavanyaJay <lavanyajay75@gmail.com>
2019-11-06 11:09:35 +0100

    Adding db.js file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+
+const db = require("./db");
+
 app.listen(process.env.PORT || port, () =>
 	console.log("Listening on port:" + port)
 );
