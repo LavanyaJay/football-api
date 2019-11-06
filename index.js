6e16559675ce31cb09ed0b5170d73e2bd675c2aa
LavanyaJay <lavanyajay75@gmail.com>
2019-11-06 11:39:52 +0100

    Adding router changes

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const db = require("./db");
+//const db = require("./db");
 
-const team = require("./team/model");
+//const Team = require("./team/model");
+
+const teamRouter = require("./team/router");
+
+app.use(teamRouter);
+
 app.listen(process.env.PORT || port, () =>
 	console.log("Listening on port:" + port)
 );
